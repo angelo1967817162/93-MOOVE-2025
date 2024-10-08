a230d0aafdb746f6c05c59ef8afbdf46cdec26ff
cisse000 <salimata2023etude@gmail.com>
2024-10-08 17:38:29 +0200

    essai sali page connexion

---
+++ src/main/java/com/example/demo/Connexion.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class Connexion {
+    public String Utilisateur;
+    public String motDePasse;
+
+    public Connexion(String Connexiono,String motDePasse){
+        this.Utilisateur = Utilisateur;
+        this.motDePasse =motDePasse;
+    }
+
+
+    public String getUtilisateur() {
+        return Utilisateur;
+    }
+    public String getmotDePasse(){
+        return motDePasse;
+    }
+
+
+
+
+}
