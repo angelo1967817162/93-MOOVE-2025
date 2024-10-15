8650beeb99547971465f010511412dc2331c6940
banu005 <banumathey5@gmail.com>
2024-10-15 16:51:50 +0200

    modifs professeur.java et professeurRepository

--- src/main/java/com/example/demo/Professeur.java
+++ src/main/java/com/example/demo/Professeur.java
@@ -1,4 +1,13 @@
 package com.example.demo;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Professeur {
+    @Id
+    private Integer id;
+    private String nom;
+    private String email;
+    private String mot_de_passe;
 }
--- src/main/java/com/example/demo/ProfesseurRepository.java
+++ src/main/java/com/example/demo/ProfesseurRepository.java
@@ -1,4 +1,13 @@
-package com.example.demo;
+package com.example.demo.repository;
 
-public interface ProfesseurRepository {
+import com.example.demo.Professeur;
+import com.example.demo.model.Tatoueur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProfesseurRepository extends JpaRepository<Professeur,Integer> {
+    List<Professeur> findByStyle(String style);
 }
