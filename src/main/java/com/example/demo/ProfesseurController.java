c697eeafa16a39e9ed60d40345b0f6a6c26bf20f
cisse000 <salimata2023etude@gmail.com>
2024-10-15 16:48:54 +0200

    modif sali control prof

--- src/main/java/com/example/demo/ProfesseurController.java
+++ src/main/java/com/example/demo/ProfesseurController.java
@@ -1,4 +1,25 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 public class ProfesseurController {
+    @Autowired
+    private ProfesseurRepository professeurRepository;
+
+    @GetMapping("/adherent")
+    public List<Professeur> professeur(){
+        return this.professeurRepository.findAll();;
+    }
+
+    @PostMapping("/professeur")
+    public void ajouterprofesseur(@RequestBody Adherent nouveauAdherent){
+//partie à écrire
+        ListeProfesseur.add(nouveauAdherent);
+    }
+
 }
