2fb4a59c2fd277b6d406e477e7ea984640ca10ca
banu005 <banumathey5@gmail.com>
2024-10-15 15:27:59 +0200

    nouvelle version

--- src/main/java/com/example/demo/Adherent.java
+++ src/main/java/com/example/demo/Adherent.java
@@ -1,30 +1,58 @@
 package com.example.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Adherent {
-    public int Id;
-    public String Utilisateur;
-    public String MotDePasse;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
 
-    public Adherent( int Id, String Utilisateur, String motDePasse){
-        this.Id = Id;
-        this.Utilisateur = Utilisateur;
-        this.MotDePasse =motDePasse;
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", name='" + getName() + "'" +
+                ", email='" + getEmail() + "'" +
+                ", password='" + getPassword() + "'" +
+                "}";
     }
 
-    public int getId()
-    {
-        return Id;
-    }
-    public String getUtilisateur()
-    {
-        return Utilisateur;
-    }
-    public String getMotDePasse(){
-        
-        return MotDePasse;
+    public String getPassword() {
+        return this.password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
+    public Integer getId() {
+        return this.id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
