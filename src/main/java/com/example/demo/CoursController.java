066801b7544e2ed6ba95adf1e3811cd42adfb65f
angelo1967817162 <angelo1967817162@gmail.com>
2024-10-15 16:53:40 +0200

    nouvcajvg adherent

--- src/main/java/com/example/demo/CoursController.java
+++ src/main/java/com/example/demo/CoursController.java
@@ -1,15 +1,16 @@
 package com.example.demo;
-import java.util.ArrayList;
+/*import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CoursController {
     // private static final String template = "Hello, %s!";
     //private final AtomicLong counter = new AtomicLong();
-    /*private List<Cours> listeCours = new ArrayList<>();*/
+    /*private List<Cours> listeCours = new ArrayList<>();
     private List<Cours> ListeCours = Arrays.asList(
                  new Cours("danses", "dessin","couture"),
                  new Cours("arts martiaux", "musique","broderie"),
@@ -38,6 +39,94 @@ public class CoursController {
 
             return this.listeCours.get(Integer.parseInt(Sport));
         }
-    }*/
+    }
+   @PutMapping("/cours/{id}")
+   public Cours modifierCours(@RequestBody Cours coursAModif, @PathVariable int id)
+           throws ChangeSetPersister.NotFoundException {
+       Cours c = this.ListeCours.get(id).Artisanat(() -> new
+               ChangeSetPersister.NotFoundException("Tatoueur non trouvé"));
+       c.getArtisanat(coursAModif.Artisanat);
+       t.setStyle(tatoueurAModif.getStyle());
+       return this.tatoueurRepository.save(t);
+   }
+    @DeleteMapping("/cours/{id}")
+    public void supprimerTatoueur(@PathVariable int id) throws ChangeSetPersister.NotFoundException {
+        Tatoueur t = this.tatoueurRepository.findById(id).orElseThrow(() -> new
+                ChangeSetPersister.NotFoundException("Tatoueur non trouvé"));
+        this.tatoueurRepository.delete(t);
+    }
+
+}*/
+import com.sun.management.UnixOperatingSystemMXBean;
+import org.springframework.web.bind.annotation.*;
+
+//import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+@RestController
+public class CoursController {
+    private List<Cours> listeCours = new ArrayList<>();
+
+    @PostMapping("/cours")
+    public List<Cours> ajouterCours(@RequestBody Cours nouveauCours){
+        this.listeCours.add(nouveauCours);
+        return listeCours;
+    }
+
+    @GetMapping("/cours")
+    public List<Cours> getListeCours() {
+        return this.listeCours;
+    }
+
+    @GetMapping("/cours/{Sport}")
+    public Cours afficherCours(@PathVariable String Sport) {
+//Cherchez le tatoueur ou la tatoueuse dans votre liste et retourner l’objet trouvé.
+        //faire un foreach sur listeTatoueurs
+        //Pour chaque Tatoueur dans chaque case, vérifiez si getId==id
+        // si oui, retrouver le tatoueur
+        for (Cours t: this.listeCours){
+            if(t.getSport()== t.Sport){
+                return t;
+            }
+        }
+        return null;
+    }
+
+    @PutMapping("/cours/{Sport}")
+    public void modifierCours(@RequestBody Cours cours, @PathVariable String Sport) {
+        //parourir la liste, utiliser la fonction set
+        for (int i = 0; i < this.listeCours.size(); i++) {
+            if (this.listeCours.get(i).getSport() == Sport) {
+                this.listeCours.set(i, cours);
+            }
+        }
+// autre manière de faire le @PutMapping
+       /* for(Tatoueur t : this.listeTatoueurs){
+            if(t.getId()==id){
+                t.setNom(tatoueurAModif.getNom());
+                t.setStyle(tatoueurAModif.getStyle());
+            }
+        }
+        */
+    }
+    @DeleteMapping("/cours/{Sport}")
+    public void supprimerCours(@PathVariable String Sport) {
+        for (int i = 0; i < this.listeCours.size(); i++) {
+            if (this.listeCours.get(i).getSport() == Sport) {
+                this.listeCours.remove(i);
+            }
+
+        }
+    }
+
+    @DeleteMapping("/cours")
+    public void supprimerCours() {
+        //Supprime tout
+        //liste.
+        this.listeCours.clear();
+    }
 
 }
