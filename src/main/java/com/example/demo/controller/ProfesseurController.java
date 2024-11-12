89b23e6d20ad172bbe10180a5eef2227c431dd4a
cisse000 <salimata2023etude@gmail.com>
2024-11-12 17:53:17 +0100

    changement adehrent

--- src/main/java/com/example/demo/controller/ProfesseurController.java
+++ src/main/java/com/example/demo/controller/ProfesseurController.java
@@ -5,13 +5,13 @@ import com.example.demo.model.Professeur;
 import com.example.demo.repository.ProfesseurRepository;
 import com.example.demo.service.ProfesseurService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
 public class ProfesseurController {
     @Autowired
     private ProfesseurRepository professeurRepository;
