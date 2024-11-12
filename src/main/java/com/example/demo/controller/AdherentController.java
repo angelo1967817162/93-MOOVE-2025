ef63450eaa889c204b0f0cc8c79c433321a4d255
angelo1967817162 <angelo1967817162@gmail.com>
2024-11-12 14:20:01 +0100

    nouveau version

--- src/main/java/com/example/demo/controller/AdherentController.java
+++ src/main/java/com/example/demo/controller/AdherentController.java
@@ -1,56 +1,74 @@
 package com.example.demo.controller;
 
 
-import com.example.demo.repository.AdherentRepository;
 import com.example.demo.model.Adherent;
+import com.example.demo.repository.AdherentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+
 public class AdherentController {
+
     @Autowired
     private AdherentRepository adherentRepository;
 
-    @GetMapping("/adherent")
-    public List<Adherent> adherent(){
-        return (List<Adherent>) this.adherentRepository.findAll();
-    }
 
-    @PostMapping("/adherent")
-    public void ajouterAdherent(@RequestBody Adherent nouveauAdherent){
-//partie à écrire
-        //ListeAdherent.add(nouveauAdherent);
-       // this.adherentRepository.save();
-
-
-
-
-
-
+    @GetMapping("/ad")
+    public List<Adherent> getAllAdherents() {
+        return (List<Adherent>) adherentRepository.findAll();
     }
 
 
+    @PostMapping
+    public Adherent createUser(@RequestBody Adherent adherent) {
+        return adherentRepository.save(adherent);
+    }
 
 
+    @GetMapping("/{id}")
+    public Adherent getAdherentById(@PathVariable Integer id) {
+        return adherentRepository.findById(id).orElse(null);
+    }
 
 
+    @PutMapping("/{id}")
+    public Adherent updateAdherent(@PathVariable Integer id, @RequestBody Adherent updatedAdherent) {
+        return adherentRepository.findById(id).map(adherent -> {
+            adherent.setNom(updatedAdherent.getNom());
+            adherent.setEmail(updatedAdherent.getEmail());
+            adherent.setMot_de_passe(updatedAdherent.getMot_de_passe());
+            return adherentRepository.save(adherent);
+        }).orElse(null);
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+    @DeleteMapping("/{id}")
+    public void deleteAdherent(@PathVariable Integer id) {
+        adherentRepository.deleteById(id);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
